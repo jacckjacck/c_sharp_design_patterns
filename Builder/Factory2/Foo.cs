cbf066fbb376f4d3ed00e4a06239a26a37c9f835
jorgea-gomez <jorge.gomez@weldnorthed.com>
2021-11-17 23:02:44 -0500

    Factory Method Async

---
+++ Builder/Factory2/Foo.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace Factory2
+{
+	class Foo
+	{
+		private Foo()
+		{
+
+		}
+
+		public static Task<Foo> CreateAsync()
+		{
+			var result = new Foo();
+			return result.InitAsync();
+		}
+
+		private async Task<Foo> InitAsync() {
+			await Task.Delay(1000);
+			return this;
+		}
+	}
+}
---
+++ Builder/Factory2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Factory2
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			Foo x = await Foo.CreateAsync();
+		}
+	}
+}

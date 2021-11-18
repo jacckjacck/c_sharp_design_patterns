725f7a32cab255046966de3e91cbfb4efc0b5785
jorgea-gomez <jorge.gomez@weldnorthed.com>
2021-11-17 22:44:57 -0500

    Factory Method

---
+++ Builder/Factory/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Factory
+{
+	internal class Point
+	{
+		private double x, y;
+
+		// Factory Method
+		public static Point NewCartesianPoint(double x, double y)
+		{
+			return new Point(x, y);
+		}
+
+		public static Point NewPolarPoint(double rho, double theta)
+		{
+			return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+		}
+
+		private Point(double x, double y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public override string ToString()
+		{
+			return $"{nameof(x)} : {x}, {nameof(y)} : {y }";
+		}
+	}
+}
---
+++ Builder/Factory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace Factory
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var point = Point.NewPolarPoint(1.0, Math.PI/2);
+
+			WriteLine(point);
+		}
+	}
+}

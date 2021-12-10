ebb6481a35d7ae7637bc26e13bd84d8809198b7a
jorgea-gomez <jorge.gomez@weldnorthed.com>
2021-12-09 19:26:35 -0500

    Use ICloneable interface

---
+++ Builder/Prototype/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Prototype
+{
+	public class Person : ICloneable
+	{
+		public string[] Names { get; set; }
+		public Address Address { get; set; }
+
+		public Person(string[] names, Address address)
+		{
+			Names = names;
+			Address = address;
+		}
+
+		public override string ToString()
+		{
+			return $" {nameof(Names)} : { string.Join(' ', Names)}, {nameof(Address)} : {Address}";
+		}
+
+		public object Clone()
+		{
+			return new Person((string[]) Names.Clone(), (Address) Address.Clone());
+		}
+	}
+
+	public class Address: ICloneable
+	{
+		public string StreetName { get; set; }
+		public int HouseNumber { get; set; }
+
+		public Address(string streetName, int houseNumber)
+		{
+			StreetName = streetName;
+			HouseNumber = houseNumber;
+		}
+
+		public override string ToString()
+		{
+			return $"{nameof(StreetName) }: {StreetName}, {nameof(HouseNumber)} :{HouseNumber} ";
+		}
+
+		public object Clone()
+		{
+			return new Address(StreetName, HouseNumber);
+		}
+	}
+}
---
+++ Builder/Prototype/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Prototype
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var john = new Person(new string[] { "John", "Doe" }, new Address("London Street", 123));
+			//var jane = john;
+			//jane.Names[0] = " Jane";
+			var jane = (Person)john.Clone();
+			jane.Names[0] = "Jane";
+			jane.Address.HouseNumber = 321;
+
+			WriteLine(john);
+			WriteLine(jane);
+		}
+	}
+}
